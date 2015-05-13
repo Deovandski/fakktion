d882ae5671ce49b196276875900c6c610232640c
Deovandski <deovandski@outlook.com>
2015-05-12 19:31:18 -0500

    genreSelected Property is now working, but having issues with displaying an updated post filtered model...

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -4,25 +4,26 @@
 App.IndexController = Ember.ObjectController.extend({
   needs: ['application'],
   genreSelected: 0,
-    
   //For Future Use when SearchByTopic is implemented// daFilter: 'blue',
   //  filtered: function(){
   //  return this.get('model').filterBy('color', this.get('daFilter'));
   //}.property('model.@each.color', 'daFilter')
   filteredPosts: function(){
-    //this.get('controllers.ApplicationController.genreSelected')
+    //controllers DEBUG console.log(this.get('controllers'));
+    //Get variables from ApplicationController
+    this.set('genreSelected', this.get('controllers.application.genreSelected'));
     console.log('Filter called');
     console.log('SelectedGenre DEBUG: ' + this.get('genreSelected'));
       if(this.get('genreSelected') != 0)
       {
-        return this.get('posts').filterBy('genre_id', this.get('genreSelected'));      
         console.log('Filter called. Result: Return all Posts with genre_id of' + this.get('genreSelected'));
+        return this.get('posts').filterBy('genre_id', this.get('genreSelected'));      
       }
       else
       {
-        return this.get('posts');
         console.log('Filter called. Result: Return all Posts');
+        return this.get('posts');
       }
-  }.observes('controllers.ApplicationController.genreSelected').property()
+  }.property('controllers.application.genreSelected')
   //.property('model.@each.color', 'daFilter')
 });
