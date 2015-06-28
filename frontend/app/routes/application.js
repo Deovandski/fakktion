c557e4149770bd71d832f855bc67c6b73e5d50f4
Deovandski <deovandski@outlook.com>
2015-06-28 00:40:04 -0400

    all deprecations and errors fixed. Assets pipeline is working, JSON is working and middleware is responding.

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
  
-export default Ember.Route.extend({
-  model: function() {
-      return Ember.Object.create({
-         genres: this.store.findAll('genre'),
-         factTypes: this.store.findAll('factType'),
-         categories: this.store.findAll('category')
-      })
-  }
+export default Ember.Route.extend
+({
+	model: function()
+	{
+		return Ember.Object.create
+		({
+			genres: this.store.findAll('genre'),
+			factTypes: this.store.findAll('factType'),
+			categories: this.store.findAll('category')
+		});
+	}
 });
