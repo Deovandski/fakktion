aee0affcc8082b7d9af202a05d5e736db6b8eaab
Deovandski <deovandski@outlook.com>
2015-07-14 20:16:10 -0400

    Travis CI: Underlying testing foundation complete

--- test/controllers/admin_messages_controller_test.rb
+++ test/controllers/admin_messages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class AdminMessagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "the truth" do
+		assert true
+	end
 end
