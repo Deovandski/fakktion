9b21420ebdd9c12878c40850eddfda55073b0058
Deovandski <deovandski@outlook.com>
2015-07-19 13:27:20 -0400

    Rails cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
   include LocalTimeHelper
-  
-  def body_class
-    body_class = @body_class || "default"
-    body_class += " " + controller.controller_name.dasherize
-    body_class += " " + controller.action_name.dasherize
-    body_class
-  end
 end
