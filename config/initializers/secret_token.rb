d150dd6d24d99a040181fd58933feafb59f09938
Deovandski <deovandski@outlook.com>
2016-01-16 23:00:31 -0600

    Heroku Security Setup #4

--- config/initializers/secret_token.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
-else
-  ENV['SECRET_KEY_BASE']
-end
