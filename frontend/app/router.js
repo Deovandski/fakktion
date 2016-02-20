47a92acb1d270c573d8321dd2ad1d0d1fd631efb
Deovandski <deovandski@outlook.com>
2016-02-20 17:31:04 -0600

    deserialization part 2 - Refactor frontend

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,93 +1,94 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend
-({
-    location: config.locationType
+var Router = Ember.Router.extend ({
+	location: config.locationType
 });
 
-Router.map(function()
-{
-  // Login related
-  this.route('header');
-  this.route('login', { path: '/login' });
+/* ROUTING INFO
+ * Dogs (View all dogs or create one)
+ * Dog (View, edit or delete info pertaining to a doge.)
+ * Index routes with dynamic slugs (dog_id) needs to be properly identified...
+ */
+Router.map(function() {
+	// Login related
+	this.route('header');
+	this.route('login', { path: '/login' });
 
-  // Admin and Super User related
-  this.route('adminPanel', { path: '/adminPanel' });
-  this.route('superUserPanel', { path: '/superUserPanel' });
+	// Admin and Super User related
+	this.route('adminPanel', { path: '/adminPanel' });
+	this.route('superUserPanel', { path: '/superUserPanel' });
 
-  // User
-  this.route('users', function(){
-      this.route('user', { path:'/:user_id' }, function()
-      {
-          this.route('edit');
-          this.route('posts');
-          this.route('comments');
-      });
-      this.route('create');
+	// USER(S) ROUTES
+	this.route('users', function(){
+		this.route('create');
+	});
+	this.route('user', function() {
+		this.route('index', { path:'/:user_id' });
+		this.route('edit');
+		this.route('posts'); // view all posts by x User
+		this.route('comments'); // View all Comments by x User
 	});
 
-  // Admin Messages
-  this.route('adminMessages', function(){
-      this.route('adminMessage', { path:'/:admin_message_id' }, function()
-      {
-          this.route('edit');
-      });
-      this.route('create');
+	// ADMIN MESSAGE(S) ROUTES
+	this.route('adminMessages', function(){
+		this.route('create');
+	});
+	this.route('adminMessage', function() {
+		this.route('index', { path:'/:admin_message_id' });
+		this.route('edit');
 	});
 
-  // Posts
-  this.route('posts', function(){
-      this.route('post', { path:'/:post_id' }, function()
-      {
-          this.route('edit');
-          // Comments
-          this.route('comments', function(){
-              this.route('comment', { path:'/:comment_id' }, function()
-              {
-                  this.route('edit');
-              });
-              this.route('create');
-          });
-      });
-      this.route('create');
+	// POST(S) ROUTES
+	this.route('posts', function(){
+		this.route('create');
+	});
+	this.route('post', function() {
+		this.route('index', { path:'/:post_id' });
+		this.route('edit');
+		// Comments related to x Post
+		this.route('comments', function(){
+			this.route('comment', { path:'/:comment_id' }, function() {
+				this.route('edit');
+			this.route('create');
+			});
+		});
 	});
 
-  // Genres
-  this.route('genres', function(){
-      this.route('genre', { path:'/:genre_id' }, function()
-      {
-          this.route('edit');
-      });
-      this.route('create');
+	// GENRE(S) ROUTES
+	this.route('genres', function(){
+		this.route('create');
 	});
-
-  // Fact Type
-  this.route('factTypes', function(){
-      this.route('factType', { path:'/:factType_id' }, function()
-      {
-          this.route('edit');
-      });
-      this.route('create');
+	this.route('genre', function(){
+		this.route('index', { path:'/:genre_id' });
+		this.route('edit');
 	});
-
-  // Topic
-  this.route('topics', function(){
-      this.route('topic', { path:'/:topic_id' }, function()
-      {
-          this.route('edit');
-      });
-      this.route('create');
+	
+	// Fact Type(S) ROUTES
+	this.route('factTypes', function(){
+		this.route('create');
 	});
-
-  // Footer related
-  this.route('footer');
-  this.route('about');
-  this.route('legal_info');
-  this.route('privacy_info');
-  this.route('support');
-  this.route('about');
-  this.route('adminMessages');
+	this.route('factType', function(){
+		this.route('index', { path:'/:factType_id' });
+		this.route('edit');
+	});
+	
+	// TOPIC(S) ROUTES
+	this.route('topics', function(){
+		this.route('create');
+	});
+	this.route('topic', function() {
+		this.route('index', { path:'/:topic_id' });
+		this.route('edit');
+	});
+	
+	// Footer related
+	this.route('footer');
+	this.route('about');
+	this.route('legal_info');
+	this.route('privacy_info');
+	this.route('support');
+	this.route('about');
 });
 
 export default Router;
