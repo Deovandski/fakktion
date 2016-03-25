7846ef73ad573cc58b1c1cce95d4eec26a5e8dcd
Deovandski <deovandski@outlook.com>
2016-03-24 19:49:39 -0500

    Post view counter now working

--- frontend/app/routes/post.js
+++ frontend/app/routes/post.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend ({
+  model(params) {
+    return this.store.findRecord('post', params.post_id);
+  },
+  setupController: function(controller, model) {
+    model.incrementProperty('views_count');
+    model.save;
+  }
 });
