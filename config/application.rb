414eb3e90f08a36c4f3cf0c166c29e25ec21c30e
Deovandski <deovandski@outlook.com>
2016-03-31 00:32:01 -0500

    closes #27

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Fakktion
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    require 'csrf_cookie_store'
   end
 end
