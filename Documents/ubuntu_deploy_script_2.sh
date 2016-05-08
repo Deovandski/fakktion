bd3738d0bfafede20e1c61a062302dd1567cd38a
Deovandski Skibinski Junior <Deovandski@users.noreply.github.com>
2016-05-08 00:33:46 -0500

    Update ubuntu_deploy_script_2.sh

--- Documents/ubuntu_deploy_script_2.sh
+++ Documents/ubuntu_deploy_script_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Overwrite NGINX configs.
 echo ""> /etc/nginx/sites-available/default
-cat Documents/fakktion.conf >> /etc/nginx/sites-available/default
+cat /home/$USER/Fakktion/Documents/fakktion.conf >> /etc/nginx/sites-available/default
 sudo service nginx restart
