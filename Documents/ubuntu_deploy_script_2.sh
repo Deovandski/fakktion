3b193a95f870852cf70df6f0e62e324115184b7a
Deovandski <deovandski@outlook.com>
2016-05-07 21:18:47 -0500

    more bash!

--- Documents/ubuntu_deploy_script_2.sh
+++ Documents/ubuntu_deploy_script_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # TODO comments
 cd /home/$USER/Fakktion
-> config/puma.rb
-cat "workers $(grep -c processor /proc/cpuinfo)" >> config/puma.rb
+echo "" > config/puma.rb
+echo "workers $(grep -c processor /proc/cpuinfo)" >> config/puma.rb
 cat Documents/partial_puma.txt >> config/puma.rb
 mkdir -p shared/pids shared/sockets shared/log
 cd ~
--- Documents/ubuntu_deploy_script_3.sh
+++ Documents/ubuntu_deploy_script_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # TODO comments
->> /etc/nginx/sites-available/default
+echo ""> /etc/nginx/sites-available/default
 cat Documents/fakktion.conf >> /etc/nginx/sites-available/default
 sudo service nginx restart
