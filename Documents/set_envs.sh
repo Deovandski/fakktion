4faf02ae60351371f11321ac1d61ebeae57e12d1
Deovandski <deovandski@outlook.com>
2016-05-08 01:37:03 -0500

    set_envs.sh escaped double quotes issue

--- Documents/set_envs.sh
+++ Documents/set_envs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Append ENVS
-#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-#\curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
-#source ~/.rvm/scripts/rvm
-#gem install bundler
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
+source ~/.rvm/scripts/rvm
+gem install bundler
 
-echo 'RAILS_ENV="production"' >> ~/.profile
-echo 'FAKKTION_DATABASE_USER="$USER"' >> ~/.profile
-echo 'FAKKTION_DATABASE_PASSWORD="CHANGE_ME"' >> ~/.profile
-echo 'FAKKTION_PRODUCTION_DATABASE="fakktion"' >> ~/.profile
+echo "SKB=\"$(rake secrets)\"" >> ~/.profile
+echo "RAILS_ENV=production" >> ~/.profile
+echo "FAKKTION_DATABASE_USER=\"$(rake secrets)\"" >> ~/.profile
+echo "FAKKTION_DATABASE_PASSWORD=CHANGE_ME" >> ~/.profile
+echo "FAKKTION_PRODUCTION_DATABASE=fakktion" >> ~/.profile
