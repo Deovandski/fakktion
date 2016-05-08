9ff68392791fcb5922d37c95ddf5805dbae7f448
Deovandski <deovandski@outlook.com>
2016-05-08 01:40:19 -0500

    and I added an unnecessary s... I've been working too long today

--- Documents/set_envs.sh
+++ Documents/set_envs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Append ENVS
+# Create Default ENV VARS
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
 source ~/.rvm/scripts/rvm
 gem install bundler
 
-echo "SKB=\"$(rake secrets)\"" >> ~/.profile
+echo "SKB=\"$(rake secret)\"" >> ~/.profile
 echo "RAILS_ENV=production" >> ~/.profile
-echo "FAKKTION_DATABASE_USER=\"$(rake secrets)\"" >> ~/.profile
+echo "FAKKTION_DATABASE_USER=\"$USER\"" >> ~/.profile
 echo "FAKKTION_DATABASE_PASSWORD=CHANGE_ME" >> ~/.profile
 echo "FAKKTION_PRODUCTION_DATABASE=fakktion" >> ~/.profile
