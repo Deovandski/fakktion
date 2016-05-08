0a1b045c2f078f944ddc1e19fde0a0dd28b3fe5e
Deovandski Skibinski Junior <Deovandski@users.noreply.github.com>
2016-05-08 00:43:48 -0500

    $USER is not longer an issue on step 3

--- Documents/ubuntu_deploy_script_2.sh
+++ Documents/ubuntu_deploy_script_2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Overwrite NGINX configs.
 sudo echo "" > /etc/nginx/sites-available/default
-cd /home/$USER/Fakktion/Documents
 sudo cat fakktion.conf >> /etc/nginx/sites-available/default
 sudo service nginx restart
