dea95ad35224ee188b0df72055b3f8748acd1ef6
Deovandski <deovandski@outlook.com>
2016-05-08 01:26:32 -0500

    set_envs.sh refactored

--- Documents/set_envs.sh
+++ Documents/set_envs.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Append ENVS
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
-source ~/.rvm/scripts/rvm
-gem install bundler
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#\curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
+#source ~/.rvm/scripts/rvm
+#gem install bundler
 
-cat 'SKB="$(rake secret)"' >> ~/.profile
-cat 'RAILS_ENV="production"' >> ~/.profile
-cat 'FAKKTION_DATABASE_USER="$USER"' >> ~/.profile
-cat 'FAKKTION_DATABASE_PASSWORD="CHANGE_ME"' >> ~/.profile
-cat 'FAKKTION_PRODUCTION_DATABASE="fakktion"' >> ~/.profile
+cat envs.txt >> ~/.profile
